21007d7c35aeb581660acffaa5d48d473b800205
Fayupable <90789180+Fayupable@users.noreply.github.com>
2024-07-17 09:12:13 +0300

    Update DbFunction.cs

--- MauiApp1/DbFunc/DbFunction.cs
+++ MauiApp1/DbFunc/DbFunction.cs
@@ -8,7 +8,7 @@ public class DbFunction:IDbFunctions
 {
     public void InsertDvDestekCagrilar(DvDestekCagrilar dvDestekCagrilar)
     {
-        using var connection = DbConnector.getConnection();
+        using var connection = DbConnector.GetConnection();
         var query = @"INSERT INTO Dv_Destek_Cagrilar (Tarih, Arayan, Aranan, ToplamSure, BeklemeSuresi, GorusmeSuresi, Sonuc, Tipi) 
                       VALUES (@Tarih, @Arayan, @Aranan, @ToplamSure, @BeklemeSuresi, @GorusmeSuresi, @Sonuc, @Tipi)";
         
@@ -24,18 +24,66 @@ public class DbFunction:IDbFunctions
 
         command.ExecuteNonQuery();
     }
-    public void UpdateDvDestekCagrilar(DvDestekCagrilar dvDestekCagrilar)
-    { 
-}
-    public void DeleteDvDestekCagrilar(DvDestekCagrilar dvDestekCagrilar){
+     public void UpdateDvDestekCagrilar(DvDestekCagrilar dvDestekCagrilar)
+    {
+        using var connection = DbConnector.GetConnection();
+        var query = @"UPDATE Dv_Destek_Cagrilar 
+                      SET Tarih = @Tarih, Arayan = @Arayan, Aranan = @Aranan, ToplamSure = @ToplamSure, BeklemeSuresi = @BeklemeSuresi, GorusmeSuresi = @GorusmeSuresi, Sonuc = @Sonuc, Tipi = @Tipi
+                      WHERE Id = @Id";
+        using var command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Id", dvDestekCagrilar.Id);
+        command.Parameters.AddWithValue("@Tarih", dvDestekCagrilar.Tarih);
+        command.Parameters.AddWithValue("@Arayan", dvDestekCagrilar.Arayan);
+        command.Parameters.AddWithValue("@Aranan", dvDestekCagrilar.Aranan);
+        command.Parameters.AddWithValue("@ToplamSure", dvDestekCagrilar.ToplamSure);
+        command.Parameters.AddWithValue("@BeklemeSuresi", dvDestekCagrilar.BeklemeSuresi);
+        command.Parameters.AddWithValue("@GorusmeSuresi", dvDestekCagrilar.GorusmeSuresi);
+        command.Parameters.AddWithValue("@Sonuc", dvDestekCagrilar.Sonuc);
+        command.Parameters.AddWithValue("@Tipi", dvDestekCagrilar.Tipi);
+        command.ExecuteNonQuery();
+    }     
+      public void DeleteDvDestekCagrilar(DvDestekCagrilar dvDestekCagrilar){
+        using var connection = DbConnector.GetConnection();
+        var query = "DELETE FROM Dv_Destek_Cagrilar WHERE Id = @Id";
+        using var command = new SqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Id", dvDestekCagrilar.Id);
+        command.ExecuteNonQuery();
+    }
+    public List<DvDestekCagrilar> SearchDvDestekCagrilar(string search)
+    {
+        var dvDestekCagrilarList = new List<DvDestekCagrilar>();
+        using var connection = DbConnector.GetConnection();
+        var query = "SELECT * FROM Dv_Destek_Cagrilar WHERE Arayan LIKE @Search OR Aranan LIKE @Search";
+        using var command = new SqlCommand(query, connection);
+        
+        command.Parameters.AddWithValue("@Search", "%" + search + "%");
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            var dvDestekCagrilar = new DvDestekCagrilar
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("id")),
+                Tarih = reader.GetDateTime(reader.GetOrdinal("tarih")),
+                Arayan = reader.GetString(reader.GetOrdinal("arayan")),
+                Aranan = reader.GetString(reader.GetOrdinal("aranan")),
+                ToplamSure = reader.GetTimeSpan(reader.GetOrdinal("toplamSure")),
+                BeklemeSuresi = reader.GetTimeSpan(reader.GetOrdinal("beklemeSuresi")),
+                GorusmeSuresi = reader.GetTimeSpan(reader.GetOrdinal("gorusmeSuresi")),
+                Sonuc = reader.GetInt32(reader.GetOrdinal("sonuc")),
+                Tipi = reader.GetInt16(reader.GetOrdinal("tipi"))
+            };
+
+            dvDestekCagrilarList.Add(dvDestekCagrilar);
+        }
+
+        return dvDestekCagrilarList;
 
     }
-    public List<DvDestekCagrilar> SearchDvDestekCagrilar(string search){
-        return null;
-    }
+
 
     public DvDestekCagrilar GetDvDestekCagrilarById(int id)
     {
+        return null;
 
     }
     public List<DvDestekCagrilar> GetAllDvDestekCagrilar()
@@ -92,7 +140,6 @@ public class DbFunction:IDbFunctions
 
             dvDestekCagrilarList.Add(dvDestekCagrilar);
         }
-
         return dvDestekCagrilarList;
     }
     

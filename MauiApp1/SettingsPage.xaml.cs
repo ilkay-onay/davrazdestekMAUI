662a0ca54ef660b1d55457b3cae61869a5d2d9fd
Batuhan Çetin <batuhan@email.com>
2024-08-02 11:45:28 +0300

    login screen buton and icon update

--- MauiApp1/SettingsPage.xaml.cs
+++ MauiApp1/SettingsPage.xaml.cs
@@ -15,12 +15,12 @@ namespace MauiApp1
             _databaseService = new DatabaseService("Server=192.168.100.220;Database=Dv;Encrypt=True;TrustServerCertificate=True;User Id=sa;Password=Password1;");
             Languages = new ObservableCollection<string>
             {
-                "Türkçe",
-                "Ýngilizce",
-                "Fransýzca",
-                "Ýspanyolca",
+                "TÃ¼rkÃ§e",
+                "Ä°ngilizce",
+                "Franszca",
+                "Ä°spanyolca",
                 "Almanca",
-                "Ýtalyanca",
+                "Ä°talyanca",
 
 
             };
@@ -34,17 +34,17 @@ namespace MauiApp1
             {
                 string selectedLanguage = Languages[LanguagePicker.SelectedIndex];
                 // Handle language change logic here
-                DisplayAlert("Dil ", $"Seçtiðiniz Dil : {selectedLanguage}", "Tamam");
+                DisplayAlert("Dil ", $"SeÃ§tiÄŸiniz Dil : {selectedLanguage}", "Tamam");
             }
         }
         private async void OnLicenseButtonClicked(object sender, EventArgs e)
         {
-            await DisplayAlert("Lisans", " Copyright © 2024 <copyright holders>\r\n\r\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\r\n\r\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\r\n\r\nTHE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.", "Kapat");
+            await DisplayAlert("Lisans", " Copyright ï¿½ 2024 <copyright holders>\r\n\r\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ï¿½Softwareï¿½), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\r\n\r\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\r\n\r\nTHE SOFTWARE IS PROVIDED ï¿½AS ISï¿½, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.", "Kapat");
         }
 
         private async void OnCreditsButtonClicked(object sender, EventArgs e)
         {
-            await DisplayAlert("Emeði Geçenler", "Ýlkay Onay\nBatuhan Çetin\nEnis Yaman\nAli Gür", "Kapat");
+            await DisplayAlert("EmeÄŸi GeÃ§enler", "Ä°lkay Onay\nBatuhan Ã‡etin\nEnis Yaman\nAli GÃ¼r", "Kapat");
         }
     }
 }
